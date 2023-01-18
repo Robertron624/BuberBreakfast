184ed570e8357bf56937b7544c51828a1e64abb2
Roberto Ramirez <robert.624@hotmail.com>
2023-01-18 17:40:46 -0500

    first commit

---
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using BuberBreakfast.Contracts.Breakfast;
+
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+[Route("[constroller]")]
+public class BreakfastController : ControllerBase
+{
+    [HttpPost()]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+}
